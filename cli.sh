35c014befa911424b6ebef16a368e3767ca031c5
kai zhu <kaizhu256@gmail.com>
2014-11-11 20:50:11 +0800

    squash beta branch to version 2014.10.31-20. see changelog section of README.md for changes

--- cli.sh
+++ cli.sh
@@ -10,27 +10,28 @@ shDownloadAndInstall() {
       touch $FILE_TMP.downloaded
     fi
     # install phantomjs
-    tar -xf $FILE_TMP && rm -f $FILE_LINK && ln -s $FILE_BIN $FILE_LINK
+    $FILE_UNZIP $FILE_TMP && rm -f $FILE_LINK && ln -s $FILE_BIN $FILE_LINK
   fi
 }
 
 shNpmPostinstall() {
   # this function runs npm postinstall
-  NODEJS_PLATFORM=$(node -e "process.stdout.write(process.platform)")
   TMPDIR=$(node -e "process.stdout.write(require('os').tmpdir())")
-  case $NODEJS_PLATFORM in
+  case $(node -e "process.stdout.write(process.platform)") in
   darwin)
     # download and install phantomjs
     FILE_BIN=phantomjs-1.9.8-macosx/bin/phantomjs
     FILE_LINK=phantomjs
     FILE_TMP=$TMPDIR/phantomjs-1.9.8-macosx.zip
     FILE_URL=https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.8-macosx.zip
+    FILE_UNZIP="unzip -q"
     shDownloadAndInstall || return $?
     # download and install slimerjs
     FILE_BIN=slimerjs-0.9.3/slimerjs
     FILE_LINK=slimerjs
-    FILE_TMP=$TMPDIR/slimerjs-0.9.3-mac.tar.bz2
-    FILE_URL=http://download.slimerjs.org/releases/0.9.3/slimerjs-0.9.3-mac.tar.bz2
+    FILE_TMP=$TMPDIR/slimerjs-0.9.3.zip
+    FILE_URL=http://download.slimerjs.org/releases/0.9.3/slimerjs-0.9.3.zip
+    FILE_UNZIP="unzip -q"
     shDownloadAndInstall || return $?
     ;;
   linux)
@@ -39,12 +40,14 @@ shNpmPostinstall() {
     FILE_LINK=phantomjs
     FILE_TMP=$TMPDIR/phantomjs-1.9.8-linux-x86_64.tar.bz2
     FILE_URL=https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.8-linux-x86_64.tar.bz2
+    FILE_UNZIP="tar -xjf"
     shDownloadAndInstall || return $?
     # download and install slimerjs
     FILE_BIN=slimerjs-0.9.3/slimerjs
     FILE_LINK=slimerjs
-    FILE_TMP=$TMPDIR/slimerjs-0.9.3-linux-x86_64.tar.bz2
-    FILE_URL=http://download.slimerjs.org/releases/0.9.3/slimerjs-0.9.3-linux-x86_64.tar.bz2
+    FILE_TMP=$TMPDIR/slimerjs-0.9.3.zip
+    FILE_URL=http://download.slimerjs.org/releases/0.9.3/slimerjs-0.9.3.zip
+    FILE_UNZIP="unzip -q"
     shDownloadAndInstall || return $?
     ;;
   esac
@@ -75,18 +78,12 @@ shMain() {
         'export PACKAGE_JSON_' + key.toUpperCase() + '=' + JSON.stringify(value.split('\n')[0])\
         : ':';\
     }).join(';'))") || return $?
-  # init $PATH with $CWD/node_modules/.bin
-  export PATH=$CWD/node_modules/phantomjs-lite:$CWD/node_modules/.bin:$PATH || return $?
-  # init $TMPFILE
-  TMPFILE=/tmp/tmpfile.$(openssl rand -hex 8) || return $?
   # eval argv
   $@
   # init $EXIT_CODE
   local EXIT_CODE=$? || return $?
   # restore $CWD
   cd $CWD || return $?
-  # cleanup $TMPFILE
-  rm -f $TMPFILE || return $?
   # return $EXIT_CODE
   return $EXIT_CODE
 }
